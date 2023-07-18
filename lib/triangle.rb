3c9cdc47230abd3a0bd84c041b9345279630136a
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-18 22:20:56 +0300

    Done assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    sides_larger_than_zero = side1 > 0 && side2 > 0 && side3 > 0
+    triangle_inequality = (side1 + side2 > side3) && (side2 + side3 > side1) && (side3 + side1 > side2)
+  
+    if sides_larger_than_zero && triangle_inequality
+      if side1 == side2 && side2 == side3
+        :equilateral
+      elsif side1 == side2 || side2 == side3 || side1 == side3
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+  
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle"
+    end
+  end
 end
